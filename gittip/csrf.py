66a3728ad57585c8d9ad2408070080ef6dfe3b19
Cory Boyd <corytheboyd@gmail.com>
2013-05-05 17:19:08 -0700

    get account link back up to speed

--- gittip/csrf.py
+++ gittip/csrf.py
@@ -144,8 +144,10 @@ def inbound(request):
             request_csrf_token = request.headers.get('X-CSRF-TOKEN', '')
 
         if not constant_time_compare(request_csrf_token, csrf_token):
-            import ipdb; ipdb.set_trace()
-            raise Response(403, REASON_BAD_TOKEN)
+            try:    
+                import ipdb; ipdb.set_trace()
+            finally:
+                raise Response(403, REASON_BAD_TOKEN)
 
 
 def outbound(response):
